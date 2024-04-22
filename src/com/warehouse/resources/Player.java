dd3ec661fbfae371c57c0ad11cd28217e39a79b2
Vazgen Levonyan <vazgenlevonyan1898@gmail.com>
2024-04-22 07:44:04 +0400

    Introduces the Player class

---
+++ src/com/warehouse/resources/Player.java
@@ -0,0 +1,37 @@
+package com.warehouse.resources;
+
+import com.warehouse.util.Util;
+
+import java.util.*;
+
+public class Player {
+    public Player(String name) {
+        Util.validateString(name);
+        this.name = name;
+        this.warehouseMap = new HashMap<>();
+    }
+
+    private String name;
+    private Map<String, Warehouse> warehouseMap;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        Util.validateString(name);
+        this.name = name;
+    }
+
+    public Map<String, Warehouse> getWarehouseMap() {
+        return warehouseMap;
+    }
+
+    public Warehouse getWarehouse(String warehouseName) {
+        return warehouseMap.get(warehouseName);
+    }
+
+    public List<Warehouse> getAllWarehouses() {
+        return new ArrayList<>(warehouseMap.values());
+    }
+}
